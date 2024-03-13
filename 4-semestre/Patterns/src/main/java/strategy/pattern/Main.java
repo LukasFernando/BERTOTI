1b202669ea78724075b7e93cdb42c017e9da0904
LukasFernando <94874696+LukasFernando@users.noreply.github.com>
2024-03-13 19:57:17 -0300

    Add files via upload

---
+++ 4-semestre/Patterns/src/main/java/strategy/pattern/Main.java
@@ -0,0 +1,15 @@
+package strategy.pattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+
+        aluno.setStatus(new Aprovado());
+        aluno.fecharNota();
+
+        aluno.setStatus(new Reprovado());
+        aluno.fecharNota();
+
+    }
+}
